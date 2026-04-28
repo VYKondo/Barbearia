2ecfb904c0a331430bafd4779e7e695f78a2df04
VYKondo <ra144029@uem.br>
2026-04-28 18:48:16 -0300

    Delete Downloads directory

--- Downloads/app-agendamento/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-
-export default function Home() {
-  // Redireciona o usuário direto para o nosso painel assim que ele entrar no site
-  redirect('/dashboard')
-}
