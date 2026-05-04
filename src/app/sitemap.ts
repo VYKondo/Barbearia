533cb6332e96a1522655fd30b2100bf0e7267ee5
Yudi <ra144029@uem.br>
2026-05-04 19:43:01 -0300

    SEO HTML /public

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://barbeariaclassica-olive.vercel.app/"; // Alterar para o domínio real quando disponível
+  const baseUrl = "https://barbeariaclassica-olive.vercel.app"; // Removida a barra final para evitar duplicidade
 
   return [
     {
